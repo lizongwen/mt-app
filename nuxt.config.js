d74a55d33fe37da3729705dd8fa78ec2d9d4d0e5
wen <93700983@qq.com>
2019-09-25 23:31:49 +0800

    1

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,8 +23,9 @@ module.exports = {
   ** Global CSS
   */
   css: [
-	//'~assets/css/main.css',
-    'element-ui/lib/theme-chalk/index.css'
+	'element-ui/lib/theme-chalk/reset.css',
+	'element-ui/lib/theme-chalk/index.css',
+	'@/assets/css/main.css',
   ],
   /*
   ** Plugins to load before mounting the App
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class="container">
-    <div>{{msg}}</div>
-    <div>{{name}}</div>
+  <div class="page-index">
+	<el-row>
+		<el-col :span="5">
+			<emenu/>
+		</el-col>
+		<el-col :span="19">2</el-col>
+	</el-row>
+	<el-row></el-row>
   </div>
 </template>
 
 <script>
 import { mapState, mapGetters } from "vuex";
+import Emenu from '@/components/index/menu'
 export default {
-  components: {},
+  components: {
+	  Emenu
+  },
   data: () => ({
     msg: "消息"
   }),
@@ -20,5 +28,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
+@import "@/assets/css/index/index.scss"
+
 </style>
